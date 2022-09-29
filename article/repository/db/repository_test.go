5379c615a9853bbc24fd863aeb7c9354146df4b4
angelrainov <angelrainov@gmail.com>
2022-09-29 18:03:02 +0300

    git commit -m added more article repo tests

--- article/repository/db/repository_test.go
+++ article/repository/db/repository_test.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"context"
-	"fmt"
 	"github.com/angelRaynov/clean-architecture/article/repository"
 	"github.com/angelRaynov/clean-architecture/domain"
 	"github.com/stretchr/testify/assert"
@@ -82,9 +81,8 @@ func TestArticleRepository_Store(t *testing.T) {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 	//TODO: fix the test
-	query := "INSERT article SET title=\\?, content=\\?, author_id=\\?, updated_at=\\?, created_at=\\?"
+	query := "INSERT article"
 	prep := mock.ExpectPrepare(query)
-	fmt.Println(prep)
 	prep.ExpectExec().WithArgs(ar.Title, ar.Content, ar.Author.ID, ar.UpdatedAt, ar.CreatedAt).WillReturnResult(sqlmock.NewResult(12, 1))
 	a := NewArticleRepository(db)
 
@@ -92,4 +90,72 @@ func TestArticleRepository_Store(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, int64(12), ar.ID)
 
+}
+
+func TestGetByTitle(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	rows := sqlmock.NewRows([]string{"id", "title", "content", "author_id", "updated_at", "created_at"}).
+		AddRow(1, "title 1", "Content 1", 1, time.Now(), time.Now())
+
+	query := "SELECT id, title, content, author_id, updated_at, created_at FROM article WHERE title = \\?"
+
+	mock.ExpectQuery(query).WillReturnRows(rows)
+	a := NewArticleRepository(db)
+
+	title := "title 1"
+	anArticle, err := a.GetByTitle(context.TODO(), title)
+	assert.NoError(t, err)
+	assert.NotNil(t, anArticle)
+}
+
+func TestDelete(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	query := "DELETE FROM article WHERE id = \\?"
+
+	prep := mock.ExpectPrepare(query)
+	prep.ExpectExec().WithArgs(12).WillReturnResult(sqlmock.NewResult(12, 1))
+
+	a := NewArticleRepository(db)
+
+	num := int64(12)
+	err = a.Delete(context.TODO(), num)
+	assert.NoError(t, err)
+}
+
+func TestUpdate(t *testing.T) {
+	now := time.Now()
+	ar := &domain.Article{
+		ID:        12,
+		Title:     "It",
+		Content:   "Content",
+		CreatedAt: now,
+		UpdatedAt: now,
+		Author: domain.Author{
+			ID:   1,
+			Name: "King",
+		},
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	query := "UPDATE article"
+
+	prep := mock.ExpectPrepare(query)
+	prep.ExpectExec().WithArgs(ar.Title, ar.Content, ar.Author.ID, ar.UpdatedAt, ar.ID).WillReturnResult(sqlmock.NewResult(12, 1))
+
+	a := NewArticleRepository(db)
+
+	err = a.Update(context.TODO(), ar)
+	assert.NoError(t, err)
 }
